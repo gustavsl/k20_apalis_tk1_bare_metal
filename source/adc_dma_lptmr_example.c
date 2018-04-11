b28a7d34371fdb8ffa2145191add87cdf1cdf32d
Gustavo Leal <gustavo.leal@toradex.com>
2018-04-11 16:52:39 -0300

    Two ADCs working

--- source/adc_dma_lptmr_example.c
+++ source/adc_dma_lptmr_example.c
@@ -36,17 +36,14 @@
 #include "board.h"
 #include "pin_mux.h"
 #include "clock_config.h"
-//#include "MK64F12.h"
 #include "fsl_debug_console.h"
 
-/* TODO: insert other include files here. */
 #include "fsl_adc16.h"
 #include "fsl_pit.h"
 #include "fsl_dmamux.h"
 #include "fsl_edma.h"
 
-/* TODO: insert other definitions and declarations here. */
-#define B_SIZE           400u		/* Internal Buffer size */
+#define B_SIZE           12u		/* Internal Buffer size */
 #define CHANNELS         3u			/* Number of ADC channels*/
 
 #define VREFH_CH         29u		/*ADC Channels*/
@@ -55,24 +52,41 @@
 
 #define DMAChannel_0	 0u			/*DMA Channels*/
 #define DMAChannel_1	 1u
+#define DMAChannel_2	 2u
+#define DMAChannel_3	 3u
 
 /* ADC Channels array */
 uint8_t g_ADC_mux[CHANNELS] = {VREFL_CH, VREFH_CH, CHANNEL_12};
-/* Internal ADC buffer */
+/* Internal ADC buffers */
 uint16_t g_ADC0_resultBuffer[B_SIZE] = {0};
+uint16_t g_ADC1_resultBuffer[B_SIZE] = {0};
 
 /* Global Done flags*/
-volatile bool g_Adc16ConversionDoneFlag = false;
+volatile bool g_Adc0ConversionDoneFlag = false;
+volatile bool g_Adc1ConversionDoneFlag = false;
+
 volatile bool g_Transfer_Done_ch0 = false;
 volatile bool g_Transfer_Done_ch1 = false;
+
+volatile bool g_Transfer_Done_ch2 = false;
+volatile bool g_Transfer_Done_ch3 = false;
 
 /* edma handler for channels 0 and 1 */
 edma_handle_t g_EDMA_Handle_0;
 edma_handle_t g_EDMA_Handle_1;
 
+/* edma handler for channels 2 and 3 */
+edma_handle_t g_EDMA_Handle_2;
+edma_handle_t g_EDMA_Handle_3;
+
 void ADC0_IRQHandler(void)
 {
-	g_Adc16ConversionDoneFlag = true;
+	g_Adc0ConversionDoneFlag = true;
+}
+
+void ADC1_IRQHandler(void)
+{
+	g_Adc1ConversionDoneFlag = true;
 }
 
 void EDMA_Callback_0(edma_handle_t *handle, void *param, bool transferDone,
@@ -93,6 +107,24 @@ void EDMA_Callback_1(edma_handle_t *handle, void *param, bool transferDone,
 	}
 }
 
+void EDMA_Callback_2(edma_handle_t *handle, void *param, bool transferDone,
+		uint32_t tcds)
+{
+	if(transferDone) {
+		g_Transfer_Done_ch2 = true;
+	}
+}
+
+void EDMA_Callback_3(edma_handle_t *handle, void *param, bool transferDone,
+		uint32_t tcds)
+{
+	if(transferDone) {
+		/* If next line is uncommented, it will start again after 12 transfers finished */
+		EDMA_StartTransfer(&g_EDMA_Handle_3);
+		g_Transfer_Done_ch3 = true;
+	}
+}
+
 /*
  * @brief   Application entry point.
  */
@@ -104,7 +136,7 @@ int main(void)
 	BOARD_InitDebugConsole();
 
 	/****************************************
-	 * ADC Config
+	 * ADC0 Config
 	 ****************************************/
 	EnableIRQ(ADC0_IRQn);
 
@@ -135,6 +167,34 @@ int main(void)
 	ADC16_SetChannelConfig( ADC0, 0, &adc16ChannelConfigStruct);
 
 	/****************************************
+	 * ADC1 Config
+	 ****************************************/
+	EnableIRQ(ADC1_IRQn);
+	/*
+	 * adc16ConfigStruct.referenceVoltageSource = kADC16_ReferenceVoltageSourceVref;
+	 * adc16ConfigStruct.clockSource = kADC16_ClockSourceAsynchronousClock;
+	 * adc16ConfigStruct.enableAsynchronousClock = true;
+	 * adc16ConfigStruct.clockDivider = kADC16_ClockDivider8;
+	 * adc16ConfigStruct.resolution = kADC16_ResolutionSE12Bit;
+	 * adc16ConfigStruct.longSampleMode = kADC16_LongSampleDisabled;
+	 * adc16ConfigStruct.enableHighSpeed = false;
+	 * adc16ConfigStruct.enableLowPower = false;
+	 * adc16ConfigStruct.enableContinuousConversion = false;
+	 */
+	ADC16_GetDefaultConfig(&adc16ConfigStruct);
+	ADC16_Init( ADC1, &adc16ConfigStruct);
+
+	ADC16_DoAutoCalibration( ADC1);
+
+	ADC16_EnableHardwareTrigger( ADC1, true);
+	ADC16_EnableDMA( ADC1, true);
+
+	adc16ChannelConfigStruct.channelNumber = g_ADC_mux[2];
+	adc16ChannelConfigStruct.enableInterruptOnConversionCompleted = true; /* Enable the interrupt. */
+	adc16ChannelConfigStruct.enableDifferentialConversion = false;
+	ADC16_SetChannelConfig( ADC1, 0, &adc16ChannelConfigStruct);
+
+	/****************************************
 	 * LPTMR for ADC HW trigger Config
 	 ****************************************/
 	pit_config_t pitConfig;
@@ -157,23 +217,31 @@ int main(void)
 			USEC_TO_COUNT(2, CLOCK_GetFreq(kCLOCK_CoreSysClk)));
 
 	/* Configure SIM for ADC hw trigger source selection */
-	SIM->SOPT7 |= 0x84U;
+	SIM->SOPT7 |= 0x8484U;
 
 	/****************************************
 	 * DMA Config
 	 ****************************************/
 	edma_transfer_config_t transferConfig_ch0;
 	edma_transfer_config_t transferConfig_ch1;
+	edma_transfer_config_t transferConfig_ch2;
+	edma_transfer_config_t transferConfig_ch3;
 	edma_config_t userConfig;
 
 	/* Configure DMAMUX */
 	DMAMUX_Init(DMAMUX0);
 
-	DMAMUX_SetSource(DMAMUX0, DMAChannel_0, 60); /* Channel 0 Source 60: DMA always enabled */
+	DMAMUX_SetSource(DMAMUX0, DMAChannel_0, kDmaRequestMux0AlwaysOn60); /* Channel 0 Source 60: DMA always enabled */
 	DMAMUX_EnableChannel(DMAMUX0, DMAChannel_0);
 
-	DMAMUX_SetSource(DMAMUX0, DMAChannel_1, 40); /* Channel 1 Source 40: ADC COCO trigger */
+	DMAMUX_SetSource(DMAMUX0, DMAChannel_1, kDmaRequestMux0ADC0); /* Channel 1 Source 40: ADC COCO trigger */
 	DMAMUX_EnableChannel(DMAMUX0, DMAChannel_1);
+
+	DMAMUX_SetSource(DMAMUX0, DMAChannel_2, kDmaRequestMux0AlwaysOn60); /* Channel 0 Source 60: DMA always enabled */
+	DMAMUX_EnableChannel(DMAMUX0, DMAChannel_2);
+
+	DMAMUX_SetSource(DMAMUX0, DMAChannel_3, kDmaRequestMux0ADC1); /* Channel 1 Source 40: ADC COCO trigger */
+	DMAMUX_EnableChannel(DMAMUX0, DMAChannel_3);
 
 	/*
 	 * Configure EDMA one shot transfer
@@ -227,6 +295,48 @@ int main(void)
 
 	EDMA_StartTransfer(&g_EDMA_Handle_1);
 
+
+
+	EDMA_CreateHandle(&g_EDMA_Handle_3, DMA0, DMAChannel_3);
+	EDMA_SetCallback(&g_EDMA_Handle_3, EDMA_Callback_3, NULL);
+
+	EDMA_PrepareTransfer(&transferConfig_ch3, /* Prepare TCD for CH1 */
+			(uint32_t*)(ADC1->R), /* Source Address (ADC0_RA) */
+			sizeof(uint16_t), /* Source width (2 bytes) */
+			g_ADC1_resultBuffer, /* Destination Address (Internal buffer)*/
+			sizeof(g_ADC1_resultBuffer[0]), /* Destination width (2 bytes) */
+			sizeof(uint16_t), /* Bytes to transfer each minor loop (2 bytes) */
+			B_SIZE * 2, /* Total of bytes to transfer (12*2 bytes) */
+			kEDMA_PeripheralToMemory); /* From ADC to Memory */
+	/* Push TCD for CH1 into hardware TCD Register */
+	EDMA_SubmitTransfer(&g_EDMA_Handle_3, &transferConfig_ch3);
+
+	/* Link channel 1 to channel 0
+	 * when channel 1 transfer ADC data,
+	 * then channel 0 will change ADC channel*/
+	EDMA_SetChannelLink(DMA0, DMAChannel_3, kEDMA_MinorLink, DMAChannel_2);
+	EDMA_SetChannelLink(DMA0, DMAChannel_3, kEDMA_MajorLink, DMAChannel_2);
+
+	EDMA_CreateHandle(&g_EDMA_Handle_2, DMA0, DMAChannel_2);
+	EDMA_SetCallback(&g_EDMA_Handle_2, EDMA_Callback_2, NULL);
+
+	EDMA_PrepareTransfer(&transferConfig_ch2, /* Prepare TCD for CH0 */
+			&g_ADC_mux[0], /* Source Address (ADC channels array) */
+			sizeof(g_ADC_mux[0]), /* Source width (1 bytes) */
+			(uint32_t*)(ADC1->SC1),/* Destination Address (ADC_SC1A_ADCH)*/
+			sizeof(uint8_t), /* Destination width (1 bytes) */
+			sizeof(uint8_t), /* Bytes to transfer each minor loop (1 bytes) */
+			CHANNELS, /* Total of bytes to transfer (3*1 bytes) */
+			kEDMA_MemoryToPeripheral);/* From ADC channels array to ADCH register */
+	/* Push TCD for CH0 into hardware TCD Register */
+	EDMA_SubmitTransfer(&g_EDMA_Handle_2, &transferConfig_ch2);
+
+	/* If transfer will continue it will need to adjust last source and destination */
+	DMA0->TCD[2].SLAST = -1 * CHANNELS;
+	DMA0->TCD[3].DLAST_SGA = -2 * B_SIZE;
+
+	EDMA_StartTransfer(&g_EDMA_Handle_3);
+
 	/* Start the LPTimer */
 	PIT_StartTimer(PIT, kPIT_Chnl_0);
 
@@ -243,7 +353,7 @@ int main(void)
 //			g_Transfer_Done_ch0 = false;
 			if(g_Transfer_Done_ch1) {
 				i = 0;
-				PRINTF("\r\nFinished\r\n");
+				PRINTF("\r\nFinished ADC0\r\n");
 
 				for (i = 0; i < B_SIZE; i+2) {
 					PRINTF("%d \t\t", g_ADC0_resultBuffer[i++]);
@@ -253,6 +363,22 @@ int main(void)
 				}
 				g_Transfer_Done_ch1 = false;
 
+			}
+		}
+
+		if(g_Transfer_Done_ch2) {
+
+			if(g_Transfer_Done_ch3) {
+				i = 0;
+				PRINTF("\r\nFinished ADC1\r\n");
+
+				for (i = 0; i < B_SIZE; i+2) {
+					PRINTF("%d \t\t", g_ADC1_resultBuffer[i++]);
+					PRINTF("%d \t\t", g_ADC1_resultBuffer[i++]);
+					PRINTF("%d \t\t", g_ADC1_resultBuffer[i++]);
+					PRINTF("\r\n");
+				}
+				g_Transfer_Done_ch3 = false;
 			}
 		}
 	}
